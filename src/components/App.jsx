173b3a8361152c650cd32bf28a62817e5f4ed6e2
Anna Kornieieva <109692085+KornieievaAnna@users.noreply.github.com>
2023-03-22 22:01:01 +0200

    refactoring App and update package.json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,28 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { AppStyle } from './App.styled';
 import { Searchbar, Modal, Button, Loader, ImageGallery } from './index';
 
 import Notiflix from 'notiflix';
 import * as Api from 'service/api';
 
-class App extends Component {
-  state = {
-    q: '',
-    page: 1,
-    perPage: 12,
-    showModal: false,
-    modalUrl: '',
-    hits: [],
-    loading: false,
-  };
+export function App() {
+  const [q, setQ] = useState('');
+  const [page, setPage] = useState(1);
+  const [perPage, setPerPage] = useState(12);
+  const [showModal, setShowModal] = useState(false);
+  const [modalUrl, setModalUrl] = useState('');
+  const [hits, setHits] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  componentDidUpdate(prevProps, prevState) {
-    const { q, page } = this.state;
+  useEffect(() => {
+    if (q === '') {
+      return
+     }
+    setLoading(true);
+    getHits(q, page, perPage);
+  }, [q, page]);
 
-    if (prevState.q !== q || prevState.page !== page) {
-      this.setState({ isLoading: true });
-      this.getHits(this.state.q, this.state.page, this.state.perPage);
-    }
-
-    window.scrollBy(0, window.innerHeight);
-  }
-
-  getHits = async (name, page, perpage) => {
+  const getHits = async (name, page, perpage) => {
     try {
       const images = await Api.getImages(name, page, perpage);
       if (!images.length) {
@@ -35,62 +30,53 @@ class App extends Component {
           `Sorry, there are no images matching your search query "${name}". Please try again.`
         );
       }
-      this.setState(prev => ({
-        hits: [...prev.hits, ...images],
-      }));
+      setHits(prev => [...prev, ...images]);
     } catch (error) {
       console.log('error');
     } finally {
-      this.setState({ isLoading: false });
+      setLoading(false);
     }
   };
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
+  const toggleModal = () => {
+    setShowModal(!showModal);
   };
 
-  handleModalOpen = url => {
-    this.setState({ modalUrl: url });
-  };
-
-  handleFormSubmit = name => {
-    if (this.state.q === name) {
+  const handleFormSubmit = name => {
+    if (q === name) {
       Notiflix.Notify.info(
         `Your request "${name}" has already been completed! :-)`
       );
       return;
     }
-    this.setState({ hits: [], q: name, page: 1 });
+    setHits([]);
+    setQ(name);
+    setPage(1);
   };
 
-  handleLoadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
+  const handleLoadMore = () => {
+    setPage(prevState => prevState.page + 1);
   };
 
-  render() {
-    const { showModal, loading, modalUrl, hits, perPage } = this.state;
-    return (
-      <AppStyle>
-        {showModal && (
-          <Modal modalImg={modalUrl} onClose={this.toggleModal}>
-            <img src={modalUrl} alt={hits.tags} />
-          </Modal>
-        )}
-        <Searchbar onSubmit={this.handleFormSubmit} />
-        <ImageGallery
-          images={hits}
-          openModal={this.toggleModal}
-          modalUrl={this.handleModalOpen}
-        ></ImageGallery>
-        {loading && <Loader />}
-        {hits.length >= perPage && !loading && (
-          <Button loadMore={this.handleLoadMore} />
-        )}
-      </AppStyle>
-    );
-  }
+  return (
+    <AppStyle>
+      {showModal && (
+        <Modal modalImg={modalUrl} onClose={toggleModal}>
+          <img src={modalUrl} alt={hits.tags} />
+        </Modal>
+      )}
+      <Searchbar onSubmit={handleFormSubmit} />
+      <ImageGallery
+        images={hits}
+        openModal={toggleModal}
+        modalUrl={setModalUrl}
+      ></ImageGallery>
+      {loading && <Loader />}
+      {hits.length >= perPage && !loading && (
+        <Button loadMore={handleLoadMore} />
+      )}
+    </AppStyle>
+  );
 }
 
 export default App;
